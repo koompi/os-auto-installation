0062dd77e6f92779d76959ddd9982e9b627a92bc
mrrbrilliantwork@gmail.com <phalbrilliant@gmail.com>
2020-09-23 12:34:08 +0700

    updated disk counting

--- main.sh
+++ main.sh
@@ -11,16 +11,23 @@ get_disk(){
             empty 
         end
     ');
+    partitionName=$(echo $InternalDiskOnly | jq '.name' -r)
+    partitionCount=$(echo $InternalDiskOnly | jq ' .children | length ')
+    # echo $partitionName
+    # echo $partitionCount
 
-    PartitionList=($(echo $InternalDiskOnly | jq '.children[].name' -r))
-
-    for((i=0;i<${#PartitionList[@]};i++)){
-        echo "/dev/${PartitionList[$i]}"
+    for((i=0;i<$partitionCount;i++)) {
+        echo "/dev/${partitionName}${i}"
     }
+    # PartitionList=($(echo $InternalDiskOnly | jq '.children[] | length' -r))
+    # echo $InternalDiskOnly | jq '. | length' -r
+    # for((i=0;i<${#PartitionList[@]};i++)){
+    #     echo "/dev/${PartitionList[$i]}"
+    # }
 }
 
 get_ram() {
-    #                                                           B     K      M      G
+    # Get RAM size                                              B     K      M      G
     echo $(lsmem -nb -o SIZE -J | jq '[ .memory[].size ] '| jq 'add / 1024 / 1024 / 1024')
 }
 
